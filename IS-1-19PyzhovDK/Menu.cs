680dbde6aeb047bca4ef19bf86a4431020d36632
Nero Solfer <sanetto616@gmail.com>
2021-11-29 19:28:39 +0500

    задание 2

---
+++ IS-1-19PyzhovDK/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_1_19PyzhovDK
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 ab = new Form1();
+            ab.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 abo = new Form2();
+            abo.ShowDialog();
+        }
+    }
+}
