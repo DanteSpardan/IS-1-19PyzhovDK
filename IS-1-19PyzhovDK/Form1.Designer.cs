63ab7f208e076b40919b7fbf74b520ca47377d8f
Nero Solfer <sanetto616@gmail.com>
2021-11-29 18:21:35 +0500

    задание 1

--- IS-1-19PyzhovDK/Form1.Designer.cs
+++ IS-1-19PyzhovDK/Form1.Designer.cs
@@ -29,13 +29,150 @@ namespace IS_1_19PyzhovDK
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(773, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(748, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(866, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(748, 38);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(866, 38);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 4;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(748, 64);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 5;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(866, 64);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 6;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(748, 90);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 7;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(866, 90);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 8;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(808, 116);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 9;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 15);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(730, 394);
+            this.listBox2.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(866, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1001, 431);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- IS-1-19PyzhovDK/Form1.cs
+++ IS-1-19PyzhovDK/Form1.cs
@@ -16,5 +16,111 @@ namespace IS_1_19PyzhovDK
         {
             InitializeComponent();
         }
+        abstract class komplekt<T>
+        {
+            public T article;
+            public int price;
+            public int agev;
+            public komplekt(T art, int pr, int agv)
+            {
+                article = art;
+                price = pr;
+                agev = agv;
+            }
+            public abstract void Display(ListBox lb);
+        }
+        class ChildProcessor<T> : komplekt<T>
+        {
+            private int chastota;
+            public int chastota1
+            {
+                get { return chastota; }
+                set { chastota = value; }
+            }
+            private int yadra;
+            public int yadra1
+            {
+                get { return yadra; }
+                set { yadra = value; }
+            }
+            private int potoki;
+            public int potoki1
+            {
+                get { return potoki; }
+                set { potoki = value; }
+            }
+
+            public ChildProcessor(T art, int price, int age, int gz, int yad, int potok)
+            : base(art, price, age)
+            {
+                chastota1 = gz;
+                yadra1 = yad;
+                potoki1 = potok;
+            }
+            public override void Display(ListBox lb)
+            {
+                lb.Items.Add($"Артикул товара : {article}");
+                lb.Items.Add($"Цена товара : {price}");
+                lb.Items.Add($"Год выпуска товара : {agev}");
+                lb.Items.Add($"Тактовая частота процессора :{chastota1}");
+                lb.Items.Add($"Количество ядер процессора :{yadra1}");
+                lb.Items.Add($"Количество потоков процессора :{potoki1}");
+            }
+            
+        }
+        class Vidyaha<T> : komplekt<T>
+        {
+            private int GPU;
+            public int GPU1
+            {
+                get { return GPU; }
+                set { GPU = value; }
+            }
+
+            private string perfom;
+            public string perfom1
+            {
+                get { return perfom; }
+                set { perfom = value; }
+            }
+
+            private int pamyat;
+            public int pamyat1
+            {
+                get { return pamyat; }
+                set { pamyat = value; }
+            }
+
+            public Vidyaha(T art, int price, int age, int gpu, string proiz, int GB)
+            : base(art, price, age)
+            {
+                GPU1 = gpu;
+                perfom1 = proiz;
+                pamyat1 = GB;
+            }
+            public override void Display(ListBox lb)
+            {
+                lb.Items.Add($"Артикул товара : {article}");
+                lb.Items.Add($"Цена товара : {price}");
+                lb.Items.Add($"Год выпуска товара : {agev}");
+                lb.Items.Add($"Частота видеочипа :{GPU1}");
+                lb.Items.Add($"Фирма производитель :{perfom1}");
+                lb.Items.Add($"Объём памяти :{pamyat1}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            ChildProcessor<string> cp1 = new ChildProcessor<string>(textBox3.Text, Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), Convert.ToInt32(textBox7.Text), Convert.ToInt32(textBox8.Text));
+            cp1.Display(listBox2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            Vidyaha<string> vid1  = new Vidyaha<string>(textBox3.Text, Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), textBox7.Text, Convert.ToInt32(textBox8.Text));
+            vid1.Display(listBox2);
+        }
     }
 }
